2ea04226e0e04c8cf7e802b5cd962bee60727a51
Fakile Razaq <razaqjimmie91@gmail.com>
2024-01-08 16:02:08 -0800

    Replace an element at a specific index

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -6,8 +6,6 @@ def replace_in_list(my_list, idx, element):
     if idx >= len(my_list):
         return my_list
     else:
-        for i in my_list:
-            if i == idx:
-                my_list[i] = element
+        my_list[idx] = element
     return my_list
 
